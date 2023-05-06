457d0f4333d0c65f1f7aa40bd3b6559cb361d21d
cxlg66you <cxlg66you@gmail.com>
2023-05-06 20:21:22 +0800

    Update is_pro.dart

--- lib/configs/is_pro.dart
+++ lib/configs/is_pro.dart
@@ -1,14 +1,14 @@
 import 'package:event/event.dart';
 import '../basic/methods.dart';
 
-var isPro = false;
+var isPro = true;
 var isProEx = 0;
 
 final proEvent = Event();
 
 Future reloadIsPro() async {
   final p = await methods.isPro();
-  isPro = p.isPro;
-  isProEx = p.expire;
+  isPro = true;
+  isProEx = 0;
   proEvent.broadcast();
 }
